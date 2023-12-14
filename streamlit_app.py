d2568ec43d69ec01a6e48e3ae40b7d8ab068de3c
harisreee <153648255+harisreee@users.noreply.github.com>
2023-12-14 13:51:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 
 streamlit.text('Kale ,Spinach & Rocket Smoothie')
 
-streamlt.text('Hard-Boiled & Free-Range Egg')
+streamlit.text('Hard-Boiled & Free-Range Egg')
 
 
 
